9c971ff67333762ba7d029cfb7a1b448deb35f77
HichamBouttaj <bouttajhicham@gmail.com>
2023-03-01 16:05:54 +0100

    backend init

---
+++ back/src/main/java/com/ghopital/projet/GestionHopitalProjetApplication.java
@@ -0,0 +1,13 @@
+package com.ghopital.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHopitalProjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionHopitalProjetApplication.class, args);
+    }
+
+}
---
+++ back/src/main/java/com/ghopital/projet/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.ghopital.projet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class WelcomeController {
+
+    @GetMapping
+    public String sayWelcome(){
+        return "Hello World Gestion Hospital Project";
+    }
+
+}
